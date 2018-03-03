acbed59ff9f735e74b923627b6082e531b39e2eb
Horst Gutmann <zerok@zerokspot.com>
2018-03-03 19:53:19 +0100

    Initial commit

---
+++ speedtest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import subprocess
+import json
+import argparse
+from influxdb import InfluxDBClient
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--influx-host', default='localhost')
+argparser.add_argument('--influx-port', default=8086, type=int)
+argparser.add_argument('--influx-username', default='root')
+argparser.add_argument('--influx-password', default='root')
+argparser.add_argument('--influx-database', default='speedtest')
+args = argparser.parse_args()
+
+client = InfluxDBClient(args.influx_host, args.influx_port, args.influx_username, args.influx_password, args.influx_database)
+
+output = subprocess.check_output('speedtest --json', shell=True)
+result = json.loads(output)
+
+client.write_points([{
+    'measurement': 'speedtest',
+    'tags': {
+        'host': result['server']['host'],
+    },
+    'fields': {
+        'upload': result['upload'],
+        'download': result['download'],
+        'ping': result['ping'],
+    },
+}])
+
