537afc39959c7ced8982647cb47c6592c6017804
Horst Gutmann <zerok@zerokspot.com>
2018-03-04 09:56:04 +0100

    Minor rendering improvements

--- render.py
+++ render.py
@@ -1,17 +1,52 @@
 import argparse
 from influxdb import InfluxDBClient
-from jinja2 import Template
+from jinja2 import Template, Environment
+import pendulum
+import humanfriendly
 
-tmpl = Template('''<!doctype html>
+def format_bytes(val):
+    return humanfriendly.format_size(val, binary=True).replace('B', 'b')
+
+def format_date(val):
+    return pendulum.parse(val).to_datetime_string() + ' UTC'
+
+env = Environment()
+env.filters['humanize_size'] = format_bytes
+env.filters['date'] = format_date
+
+tmpl = env.from_string('''<!doctype html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <style>
+        body {
+            font-family: Arial, sans serif;
+        }
+        table {
+            border: 1px solid #DDD;
+            border-spacing: 0;
+            border-bottom: 0;
+        }
+        td, th {
+            padding: 5px;
+        }
+        th {
+            background: #DDD;
+        }
+        tbody tr:nth-child(even) td {
+            background: #EFEFEF; 
+        }
+        tbody td {
+            border-bottom: 1px solid #DDD;
+        }
+        </style>
     </head>
     <body>
         <table>
             <thead>
                 <tr>
                     <th>Time</th>
+                    <th>Host</th>
                     <th>Ping</th>
                     <th>Download</th>
                     <th>Upload</th>
@@ -20,10 +55,11 @@ tmpl = Template('''<!doctype html>
             <tbody>
                 {% for point in points %}
                 <tr>
-                    <td>{{ point.time }}</td>
-                    <td>{{ point.ping }}</td>
-                    <td>{{ point.download }}</td>
-                    <td>{{ point.upload }}</td>
+                    <td>{{ point.time|date }}</td>
+                    <td>{{ point.host }}</td>
+                    <td>{{ point.ping }}ms</td>
+                    <td>{{ point.download|humanize_size }}/s</td>
+                    <td>{{ point.upload|humanize_size }}/s</td>
                 </tr>
                 {% endfor %}
             </tbody>
@@ -41,6 +77,6 @@ args = parser.parse_args()
 
 client = InfluxDBClient(args.influx_host, args.influx_port, args.influx_username, args.influx_password, args.influx_database)
 points = []
-for point in client.query('select * from speedtest where time > now() - 6h order by time desc').get_points():
+for point in client.query('select * from speedtest where time > now() - 24h order by time desc').get_points():
     points.append(point)
 print(tmpl.render(points=points))
--- speedtest.py
+++ speedtest.py
@@ -10,11 +10,12 @@ argparser.add_argument('--influx-port', default=8086, type=int)
 argparser.add_argument('--influx-username', default='root')
 argparser.add_argument('--influx-password', default='root')
 argparser.add_argument('--influx-database', default='speedtest')
+argparser.add_argument('--speedtest-bin', default='speedtest')
 args = argparser.parse_args()
 
 client = InfluxDBClient(args.influx_host, args.influx_port, args.influx_username, args.influx_password, args.influx_database)
 
-output = subprocess.check_output('speedtest --json', shell=True)
+output = subprocess.check_output('{} --json'.format(args.speedtest_bin), shell=True).decode('utf-8')
 result = json.loads(output)
 
 client.write_points([{
